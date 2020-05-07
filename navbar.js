12a6c5bfa290918c7bf4ddce66cd1822a2b8ba6b
Ninetalesgh <camillolukesch@gmail.com>
2020-05-07 21:29:29 +0200

    nav bar layout

--- navbar.js
+++ navbar.js
@@ -1,12 +1,14 @@
 document.write('\
-<nav class="navbar navbar-fixed-top">\
+<nav class="navbar navbar-fixed-top navbar-expand-sm bg-dark">\
     <div class="container-fluid">\
       <div class="navbar-header">\
-        <a class="navbar-brand" href="home.html">In Times of Innocence</a>\
+        <a class="navbar-brand" href="home.html">Home</a>\
       </div>\
-      <ul class="nav navbar-nav navbar-right">\
-        <li><a href="contact.html">Contact</a></li>\
-        <li><a href="projects.html">Projects</a></li>\
-        <li><a href="mailto:timesofinnocence@gmail.com"><span class="glyphicon glyphicon-envelope"></span></a></li>\
-        <li><a href="https://www.facebook.com/InZeitenGrosserUnschuld/"><i class="fa fa-facebook-square"></i></a></li>\
-        </ul></div></nav>');
+        <ul class="nav navbar-nav navbar-right">\
+          <li class="li-padding"><a href="contact.html">Contact</a></li>\
+          <li class="li-padding"><a href="projects.html">Projects</a></li>\
+          <li class="li-padding"><a href="mailto:timesofinnocence@gmail.com"><i class="fa fa-envelope"></i></a></li>\
+          <li><a href="https://www.facebook.com/InZeitenGrosserUnschuld/"><i class="fa fa-facebook-square"></i></a></li>\
+        </ul>\
+      </div>\
+</nav>');
