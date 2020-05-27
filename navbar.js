4389d5e714c83e2c3df65e20d2d674841d6742a2
Ninetalesgh <camillolukesch@gmail.com>
2020-05-27 12:40:14 +0200

    aufruf zur mahnwache basic page

--- navbar.js
+++ navbar.js
@@ -1,14 +1,14 @@
 document.write('\
-<nav class="navbar navbar-fixed-top navbar-expand-sm bg-dark">\
+<nav class="navbar navbar-fixed-top navbar-expand-sm itoi-navbar">\
     <div class="container-fluid">\
       <div class="navbar-header">\
-        <a class="navbar-brand" href="home.html">Home</a>\
+        <a class="navbar-brand itoi-navbar" href="index.html">Home</a>\
       </div>\
         <ul class="nav navbar-nav navbar-right">\
-          <li class="li-padding"><a href="contact.html">Contact</a></li>\
-          <li class="li-padding"><a href="projects.html">Projects</a></li>\
-          <li class="li-padding"><a href="mailto:timesofinnocence@gmail.com"><i class="fa fa-envelope"></i></a></li>\
-          <li><a href="https://www.facebook.com/InZeitenGrosserUnschuld/"><i class="fa fa-facebook-square"></i></a></li>\
+          <li class="li-padding"><a class="itoi-navbar" href="contact.html">Contact</a></li>\
+          <li class="li-padding"><a class="itoi-navbar" href="projects.html">Projects</a></li>\
+          <li class="li-padding"><a class="itoi-navbar" href="mailto:timesofinnocence@gmail.com"><i class="fa fa-envelope"></i></a></li>\
+          <li><a href="https://www.facebook.com/InZeitenGrosserUnschuld/"><i class="fa fa-facebook-square itoi-navbar"></i></a></li>\
         </ul>\
       </div>\
 </nav>');
