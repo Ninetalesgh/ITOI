c0e6c73bb4e1cc591f4861022d9d930794079efc
Ninetalesgh <camillolukesch@gmail.com>
2020-06-05 21:33:10 +0200

    style refactoring

--- include.js
+++ include.js
@@ -6,7 +6,9 @@ document.write('\
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>\
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>\
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>\
-    <link rel="stylesheet" href="style.css" />\
+    <link rel="stylesheet" href="style/itoi.css" />\
+    <link rel="stylesheet" href="style/text.css" />\
+    <link rel="stylesheet" href="style/themes.css" />\
+    <link rel="stylesheet" href="style/image.css" />\
     <script src="https://use.fontawesome.com/eae9d3e910.js"></script>\
-\
 ');
--- navbar.js
+++ navbar.js
@@ -1,14 +1,14 @@
 document.write('\
-<nav class="navbar navbar-fixed-top navbar-expand-sm itoi-navbar">\
+<nav class="navbar navbar-fixed-top navbar-expand-sm itoi-dark">\
     <div class="container-fluid">\
       <div class="navbar-header">\
-        <a class="navbar-brand itoi-navbar" href="index.html">Home</a>\
+        <a class="navbar-brand itoi-dark itoi-uppercase" href="index.html">Home</a>\
       </div>\
-        <ul class="nav navbar-nav navbar-right">\
-          <li class="li-padding"><a class="itoi-navbar" href="contact.html">Contact</a></li>\
-          <li class="li-padding"><a class="itoi-navbar" href="projects.html">Projects</a></li>\
-          <li class="li-padding"><a class="itoi-navbar" href="mailto:timesofinnocence@gmail.com"><i class="fa fa-envelope"></i></a></li>\
-          <li><a href="https://www.facebook.com/InZeitenGrosserUnschuld/"><i class="fa fa-facebook-square itoi-navbar"></i></a></li>\
+        <ul class="nav navbar-nav navbar-right itoi-dark">\
+          <li style="padding-right: 20px;"><a class="itoi-dark itoi-uppercase" href="contact.html">Contact</a></li>\
+          <li style="padding-right: 20px;"><a class="itoi-dark itoi-uppercase" href="mahnwache.html">Projects</a></li>\
+          <li style="padding-right: 20px;"><a href="mailto:timesofinnocence@gmail.com"><i class="fa fa-envelope itoi-dark"></i></a></li>\
+          <li><a href="https://www.facebook.com/InZeitenGrosserUnschuld/"><i class="fa fa-facebook-square itoi-dark"></i></a></li>\
         </ul>\
       </div>\
 </nav>');
